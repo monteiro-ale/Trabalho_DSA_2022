e08a178e7082cc685ccaf63637619200ba1e4843
monteiro-ale <monteiro.tec32@gmail.com>
2022-10-26 15:58:56 -0300

    First Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+// const produtoRota = require('./rotas/produto_rotas')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// app.use('/api/produtos', produtoRota);
+
+app.listen(3000, () => {
+    console.log("Iniciando o servidor")
+})
+
+// console.log('Olá Mundo!')
